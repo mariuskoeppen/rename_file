62f696022b0c52b3457d3a65af65b3e13ac8396d
Marius <mar.koeppen@gmail.com>
2023-02-25 12:52:35 +0100

    Files every 5 secodns

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
+use chrono::{Datelike, Timelike, Utc};
+use std::fs::rename;
 use std::path::Path;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    println!("{}", Path::new("./temp.rec").exists());
+    loop {
+        thread::sleep(Duration::from_millis(5000));
+
+        if Path::new("./temp.rec").exists() {
+            let now = Utc::now();
+
+            rename(
+                Path::new("./temp.rec"),
+                Path::new(&format!(
+                    "./replay_{}-{:02}-{:02}_{:02}-{:02}.rec",
+                    now.year(),
+                    now.month(),
+                    now.day(),
+                    now.hour(),
+                    now.minute()
+                )),
+            )
+            .expect("An error occured renaming the file. ");
+        }
+    }
 }
